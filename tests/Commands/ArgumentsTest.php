6a2882e4f5265db0cdf31e2bfbaf927348a46998
Dragon2517 <rabvasya1994@yandex.ru>
2023-05-24 23:23:47 +0300

    123

--- tests/Commands/ArgumentsTest.php
+++ tests/Commands/ArgumentsTest.php
@@ -3,6 +3,7 @@
 namespace Dragon2517\Blog\UnitTests\Commands;
 
 use Dragon2517\AdvancedPhpZero\Blog\Commands\Arguments;
+use Dragon2517\AdvancedPhpZero\Blog\Exceptions\ArgumentsException;
 use PHPUnit\Framework\TestCase;
 
 class ArgumentsTest extends TestCase
@@ -25,5 +26,50 @@ class ArgumentsTest extends TestCase
         // Можно также явно проверить,
         // что значение является строкой
         $this->assertIsString($value);
+    }
+
+    public function testItThrowsAnExceptionWhenArgumentIsAbsent(): void
+    {
+        // Подготавливаем объект с пустым набором данных
+        $arguments = new Arguments([]);
+        // Описываем тип ожидаемого исключения
+        $this->expectException(ArgumentsException::class);
+        // и его сообщение
+        $this->expectExceptionMessage("No such argument: some_key");
+        // Выполняем действие, приводящее к выбрасыванию исключения
+        $arguments->get('some_key');
+    }
+
+
+    // Провайдер данных
+    public function argumentsProvider(): iterable
+    {
+        return [
+            ['some_string', 'some_string'], // Тестовый набор
+            // Первое значение будет передано
+            // в тест первым аргументом,
+            // второе значение будет передано
+            // в тест вторым аргументом
+            [' some_string', 'some_string'], // Тестовый набор №2
+            [' some_string ', 'some_string'],
+            [123, '123'],
+            [12.3, '12.3'],
+        ];
+    }
+    // Связываем тест с провайдером данных с помощью аннотации @dataProvider
+    // У теста два агрумента
+    // В одном тестовом наборе из провайдера данных два значения
+    /**
+     * @dataProvider argumentsProvider
+     */
+    public function testItConvertsArgumentsToStrings(
+        $inputValue,
+        $expectedValue
+    ): void {
+        // Подставляем первое значение из тестового набора
+        $arguments = new Arguments(['some_key' => $inputValue]);
+        $value = $arguments->get('some_key');
+        // Сверяем со вторым значением из тестового набора
+        $this->assertEquals($expectedValue, $value);
     }
 }
